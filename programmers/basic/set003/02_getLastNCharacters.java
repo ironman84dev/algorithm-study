b38c30c63ebade89aa0096763bf251ff1f334bf4
ironman84dev <ironman84dev@gmail.com>
2026-02-04 20:58:27 +0900

    refactor: improve readability by using substring

--- programmers/basic/set003/02_getLastNCharacters.java
+++ programmers/basic/set003/02_getLastNCharacters.java
@@ -1,11 +1,6 @@
 class Solution {
     public String solution(String my_string, int n) {
-        String answer = "";
-        int len = my_string.length();
-        for(int i = len-n; i < len; i++) {
-            answer += my_string.charAt(i);
-        }
-        
-        return answer;
+         
+        return my_string.substring(my_string.length()-n);
     }
 }
